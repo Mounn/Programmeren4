28fc692e9991868aa0125d724c91db68bff51a63
TankUnit <Mounn_mocro@hotmail.com>
2018-07-04 17:35:33 +0200

    fix controller delers delete,update,create

--- node-programmeren4-studentenhuis-master/controllers/spullen.controller.js
+++ node-programmeren4-studentenhuis-master/controllers/spullen.controller.js
@@ -76,9 +76,6 @@ module.exports = {
      */
     getAll(req, res, next) {
         try {
-            const huisId = req.params.huisId
-            const query = 'SELECT ID, Naam, Beschrijving, Merk, Soort, Bouwjaar FROM spullen WHERE categorieID = ?'
-            const values = [huisId]
             pool.getConnection((err, connection) => {
                 if (err) {
                     logger.error('Error getting connection from pool: ' + err.toString())
@@ -86,15 +83,16 @@ module.exports = {
                     next(error);
                     return
                 }
-                connection.query(query, (err, rows, fields) => {
-                    connection.release()
-                    if (err) {
-                        const error = new ApiError(err, 412)
-                        next(error);
-                    } else {
-                        res.status(200).json({result: rows}).end()
-                    }
-                })
+                connection.query('SELECT * FROM spullen WHERE CategorieID = ?', [req.params.huisId],
+                    (err, rows, fields) => {
+                        connection.release()
+                        if (err) {
+                            const error = new ApiError(err, 412)
+                            next(error);
+                        } else {
+                            res.status(200).json({result: rows}).end()
+                        }
+                    })
             })
         } catch (ex) {
             logger.error(ex)
@@ -147,22 +145,157 @@ module.exports = {
      * Replace an existing object in the database.
      */
     update(req, res, next) {
-        res.status(200).json({
-            msg: 'Not implemented yet!'
-        }).end()
+        try {
+            assert(req.user && req.user.id, 'User ID is missing!')
+            assert(typeof (req.body) === 'object', 'request body must have an object containing naam and adres.')
+            assert(typeof (req.body.naam) === 'string', 'naam must be a string.')
+            assert(typeof (req.body.beschrijving) === 'string', 'beschrijving must be a string.')
+            assert(typeof (req.body.merk) === 'string', 'merk must be a string.')
+            assert(typeof (req.body.soort) === 'string', 'soort must be a string.')
+            assert(typeof (req.body.bouwjaar) === 'number', 'bouwjaar must be a number.')
+        } catch (ex) {
+            const error = new ApiError(ex.toString(), 500)
+            next(error)
+            return
+        }
+
+        const huisId = req.params.huisId
+        const spullenId = req.params.spullenId
+
+        // 1. Zoek in pool of categorie met categorieId bestaat + spullenId correct is
+        try {
+            pool.getConnection((err, connection) => {
+                if (err) {
+                    logger.error('Error getting connection from pool: ' + err.toString())
+                    const error = new ApiError(err, 500)
+                    next(error);
+                    return
+                }
+                connection.query('SELECT * FROM spullen WHERE ID = ? AND categorieID = ?', [ spullenId, huisId ],
+                    (err, rows, fields) => {
+                        connection.release()
+                        if (err) {
+                            const error = new ApiError(err, 412)
+                            next(error);
+                        } else {
+                            // rows MOET hier 1 waarde bevatten - nl. het gevonden categorie.
+                            if(rows.length !== 1) {
+                                // zo nee, dan error
+                                const error = new ApiError("Not existing row/spullen", 404)
+                                next(error);
+                            } else {
+                                // zo ja, dan
+                                // - check eerst of de huidige user de 'eigenaar' van het categorie is
+                                if(rows[0].UserID !== req.user.id) {
+                                    //  - zo nee, error
+                                    const error = new ApiError(err, 412)
+                                    next(error);
+                                } else {
+                                    //  - zo ja, dan SQL query UPDATE
+                                    pool.query(
+                                        'UPDATE spullen SET Naam = ?, Beschrijving = ?, Merk = ?, Soort = ?, Bouwjaar = ? WHERE ID = ?',
+                                        [ req.body.naam, req.body.beschrijving, req.body.merk, req.body.soort, req.body.bouwjaar, spullenId],
+                                        (err, rows, fields) => {
+                                            if(err) {
+                                                // handle error
+                                                const error = new ApiError(err, 412)
+                                                next(error);
+                                            } else {
+                                                // handle success
+                                                pool.query(
+                                                    'SELECT * FROM spullen WHERE ID = ?',
+                                                    [ spullenId ],
+                                                    (err, rows, fields) => {
+                                                        if(err) {
+                                                            // handle error
+                                                            const error = new ApiError(err, 412)
+                                                            next(error);
+                                                        } else {
+                                                            res.status(200).json({ result: rows }).end()
+                                                        }
+                                                    })
+                                            }
+                                        })
+                                }
+                            }
+                        }
+                    })
+            })
+        } catch (ex) {
+            logger.error(ex)
+            const error = new ApiError(ex, 500)
+            next(error);
+        }
     },
 
     /**
      * Delete an item.
-     *
-     * @param {object} req
-     * @param {object} res
-     * @param {function} next
      */
     delete(req, res, next) {
-        res.status(400).json({
-            msg: 'Not implemented yet!'
-        }).end()
+        try {
+            assert(req.user && req.user.id, 'User ID is missing!')
+            // More validations here.
+        } catch (ex) {
+            const error = new ApiError(ex.toString(), 500)
+            next(error)
+            return
+        }
+
+        const huisId = req.params.huisId
+        const spullenId = req.params.spullenId
+
+        try {
+            pool.getConnection((err, connection) => {
+                if (err) {
+                    logger.error('Error getting connection from pool: ' + err.toString())
+                    const error = new ApiError(err, 500)
+                    next(error);
+                    return
+                }
+                connection.query('SELECT * FROM spullen WHERE ID = ? AND categorieID = ?', [ spullenId, huisId ],
+                    (err, rows, fields) => {
+                        connection.release()
+                        if (err) {
+                            const error = new ApiError(err, 412)
+                            next(error);
+                        } else {
+                            // rows MOET hier 1 waarde bevatten - nl. het gevonden categorie.
+                            if(rows.length !== 1) {
+                                // zo nee, dan error
+                                const error = new ApiError("Niet gevonden Hij bestaat niet)", 404)
+                                next(error);
+                            } else {
+                                // zo ja, dan
+                                // - check eerst of de huidige user de 'eigenaar' van het categorie is
+                                if(rows[0].UserID !== req.user.id) {
+                                    //  - zo nee, error
+                                    const error = new ApiError("Een of meer properties in de request body ontbreken of zijn foutief", 412)
+                                    next(error);
+                                } else {
+                                    //  - zo ja, dan SQL query DELETE
+                                    pool.query(
+                                        'DELETE FROM spullen WHERE ID = ?',
+                                        [spullenId],
+                                        (err, rows, fields) => {
+                                            if(err) {
+                                                // handle error
+                                                const error = new ApiError("Een of meer properties in de request body ontbreken of zijn foutief", 412)
+                                                next(error);
+                                            } else {
+                                                // handle success
+                                                res.status(200).json({ result: rows }).end()
+                                            }
+                                        })
+                                }
+                            }
+                        }
+                    })
+            })
+        } catch (ex) {
+            logger.error(ex)
+            const error = new ApiError(ex, 500)
+            next(error);
+        }
     }
 
 }
